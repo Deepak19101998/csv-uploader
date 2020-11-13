fda1a12f784e35fb50918b95f59159b94e876da3
Deepak_gupta <dg14964@gmail.com>
2020-11-13 07:03:38 -0500

    Added some code on JSON function

--- src/Components/CreateTable.js
+++ src/Components/CreateTable.js
@@ -13,18 +13,19 @@ function CreateTable(props) {
       header: false,
       complete: function (results) {
         // console.log(results);
-        let i = 0;
-        results.data.map((data, index) => {
+        for(let i=0; i<results.data.length;i++){
           //console.log(data);
+          if(i==11){
+              break;
+          }
           if (i === 0) {
             let table = document.getElementById("tbl-data");
-            generateTableHead(table, data);
+            generateTableHead(table, results.data[i]);
           } else {
             let table = document.getElementById("tbl-data");
-            generateTableRows(table, data);
+            generateTableRows(table, results.data[i]);
           }
-          i++;
-        });
+        }
       },
     });
   };
@@ -34,6 +35,10 @@ function CreateTable(props) {
     let row = thead.insertRow();
     for (let key of data) {
       let th = document.createElement("th");
+      th.style.backgroundColor = "black";
+      th.style.color = "white";
+      th.style.padding = "8px";
+      th.style.cursor = "pointer";
       let text = document.createTextNode(key);
       th.appendChild(text);
       row.appendChild(th);
@@ -44,6 +49,10 @@ function CreateTable(props) {
     let newRow = table.insertRow();
     data.map((row, index) => {
       let newCell = newRow.insertCell();
+      newCell.style.backgroundColor = "black";
+      newCell.style.padding = "8px";
+      newCell.style.color = "white";
+      newCell.style.cursor = "pointer";
       let newText = document.createTextNode(row);
       newCell.appendChild(newText);
     });
@@ -70,12 +79,18 @@ function CreateTable(props) {
 
   return (
     <div>
-      <button id="btn-upload-csv" className="btn btn-md btn-outline-danger my-2" onClick={handleClick}>
-        Read CSV
-      </button>
-      <button id="btn-upload-json" className="btn btn-md btn-outline-danger m-2" onClick={handleClickJSON}>
-        Download JSON
-      </button>
+        {file &&
+            <button id="btn-upload-csv" className="btn btn-md btn-outline-danger my-2" onClick={handleClick}>
+                 Read CSV
+             </button>
+        }
+        {file &&
+        <button id="btn-upload-json" className="btn btn-md btn-outline-danger m-2" onClick={handleClickJSON}>
+             Download JSON
+        </button>
+      }
+      
+        
       <table id="tbl-data" className="my-3 border border-dark"></table>
       {jsonData.length > 0 && <p>{jsonData.length}</p>}
     </div>
@@ -84,67 +99,6 @@ function CreateTable(props) {
 
 export default CreateTable;
 
-// import React from "react";
-// import Papa from 'papaparse';
 
-// function CreateTable(props){
-//     const{file} = props;
-
-//      const handleClick = () =>{
-//         //console.log(file);
-//         Papa.parse(file,{
-//             download:true,
-//             worker:true,
-//             header:false,
-//             complete:function(results){
-//                 console.log(results); 
-//                 let i = 0;
-//                 results.data.map((data,index)=>{
-//                     //console.log(data); 
-//                    if(i===0){
-//                         let table = document.getElementById('tbl-data');
-//                         generateTableHead(table,data);
-//                    }
-//                    else{
-//                     let table = document.getElementById('tbl-data');
-//                     generateTableRows(table,data);
-//                    }
-//                    i++;
-//                 });
-//             }
-//         });
-//     }
-
-//     function generateTableHead(table,data){
-//         let thead = table.createTHead();
-//         let row = thead.insertRow();
-//         for(let key of data){
-//             let th = document.createElement('th');
-//             let text = document.createTextNode(key);
-//             th.appendChild(text);
-//             row.appendChild(th);
-//         }
-//     }
-
-//     function generateTableRows(table,data){
-//         let newRow = table.insertRow();
-//         data.map((row,index)=>{
-//             let newCell = newRow.insertCell();
-//             let newText = document.createTextNode(row);
-//             newCell.appendChild(newText);
-//         })
-//     }
-
-//     return(
-//         <div>
-//             <button id="btn-upload-csv" className="btn btn-md btn-outline-danger my-2" onClick={handleClick}>Read CSV</button>
-//             <table id="tbl-data" className="my-3 border border-dark">
-
-//             </table>
-//         </div>
-//     );
-// }
-
-// export default CreateTable;
 
 
